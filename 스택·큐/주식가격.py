bc28c32ba20b68bb4bfdb730d2a753d49b89ba18
Jihyeong Ko <andrew@solidware.io>
2020-12-19 17:50:55 +0900

    solved STACK/QUE

---
+++ 스택·큐/주식가격.py
@@ -0,0 +1,23 @@
+'''https://programmers.co.kr/learn/courses/30/lessons/42584'''
+
+from collections import deque
+
+def solution(prices):
+    prices = deque(prices)
+    result = list()
+
+    while True:
+        state = prices.popleft()
+        if len(prices) == 0:
+            result.append(0)
+            break
+        combo = 0
+        for price in prices:
+            combo += 1
+            if state <= price:
+                pass
+            else:
+                break
+        result.append(combo)
+
+    return result
