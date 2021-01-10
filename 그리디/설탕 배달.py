17a00bab4f8b0f31fa7c8a4453f5b6e1541816b8
Jihyeong Ko <andrew@solidware.io>
2021-01-10 12:09:33 +0900

    wip

--- 그리디/설탕 배달.py
+++ 그리디/설탕 배달.py
@@ -10,10 +10,3 @@ def get_least_bag(N):
                 return -1
             max_5 -= 1
             max_3 = (N - max_5 * 5) // 3
-
-
-N = int(input())
-print(get_least_bag(N))
-
-#%%
-import pand
