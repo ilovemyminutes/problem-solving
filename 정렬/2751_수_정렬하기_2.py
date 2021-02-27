f21e518b5c76700481bcc1471fbd4f6cb20a0fa5
Jihyeong Ko <silkstaff@naver.com>
2021-02-27 21:08:25 +0900

    solved 2751

---
+++ 정렬/2751_수_정렬하기_2.py
@@ -0,0 +1,13 @@
+'''https://www.acmicpc.net/problem/2751'''
+import sys
+
+n = int(input())
+
+numbers = []
+for _ in range(n):
+    numbers.append(int(sys.stdin.readline()))
+
+numbers.sort()
+
+for number in numbers:
+    sys.stdout.write(str(number) + '\n')
---
+++ 정렬/강의실_배정.py
@@ -0,0 +1,48 @@
+'''https://www.acmicpc.net/problem/11000
+우선순위 큐
+'''
+import sys
+import copy
+
+def get_chain(root_index: tuple, schedules: list) -> list:
+    s0, t0 = schedules.pop(root_index)
+    result = [s0]
+    while schedules[root_index:]:
+        for idx, (s1, t1) in enumerate(schedules[root_index:]):
+            if t0 <= s1:
+                result.append(s1)
+                s0, t0 = s1, t1
+                schedules.pop(root_index + idx)
+                root_index = idx
+                break
+        if idx == len(schedules[root_index:]) - 1:
+            return result
+    return result
+    
+def flatten(array: list) -> list:
+    result = []
+    for element in array:
+        if isinstance(element, list):
+            result.extend(element)
+        else:
+            result.append(element)
+    return result
+
+S, T = 0, -1
+N = int(input())
+schedules = sorted([tuple(map(int, sys.stdin.readline().split())) for _ in range(N)], key=lambda x: x[S])
+room_dict = dict()
+room_id = 0
+
+while schedules:
+    root_index = 0
+    s, t = schedules[root_index]
+    if s not in flatten(list(room_dict.values())):
+        room_dict[room_id] = get_chain(root_index, schedules)
+        room_id += 1
+    else:
+        root_index += 1
+        if root_index == len(schedules):
+            break
+
+print(len(room_dict))
