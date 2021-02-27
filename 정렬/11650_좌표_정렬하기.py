db28e3c134fa6a5de39020a4fcaa1b842a57ebe9
Jihyeong Ko <silkstaff@naver.com>
2021-02-27 21:37:48 +0900

    solved 11650, BOJ

---
+++ 정렬/11650_좌표_정렬하기.py
@@ -0,0 +1,18 @@
+'''https://www.acmicpc.net/problem/11650'''
+import sys
+
+FORMER, LATTER = 0, 1
+
+n = int(input())
+
+numbers = []
+for _ in range(n):
+    n1, n2 = sys.stdin.readline().split()
+    numbers.append((int(n1), int(n2)))
+    
+numbers.sort(key=lambda x: x[LATTER])
+numbers.sort(key=lambda x: x[FORMER])
+
+for number in numbers:
+    output = f'{number[FORMER]} {number[LATTER]}'
+    sys.stdout.write(output + '\n')
