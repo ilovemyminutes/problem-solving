d1aaea878a28c21b3f68cfd9fce680990f724cf1
Jihyeong Ko <silkstaff@naver.com>
2021-03-04 07:58:34 +0900

    solved 1436

--- brute-force/1436.py
+++ brute-force/1436.py
@@ -2,3 +2,14 @@
 종말의 숫자: 어떤 수에 6이 적어도 3개 이상 연속으로 들어가는 수
 '''
 n = int(input())
+
+SPECIAL_NUM = '666'
+title = 0
+pseudo_n = 0
+
+while n != pseudo_n:
+    title += 1
+    if SPECIAL_NUM in str(title):
+        pseudo_n += 1
+
+print(title)
