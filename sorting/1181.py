435eceb1c9f9c5c6e9820e4d1bef3974c618574f
Jihyeong Ko <silkstaff@naver.com>
2021-03-05 08:16:09 +0900

    solved 1181, BOJ

---
+++ sorting/1181.py
@@ -0,0 +1,13 @@
+'''단어 정렬, https://www.acmicpc.net/problem/1181'''
+import sys
+n = int(input())
+
+sentences = list(set([sys.stdin.readline()[:-1] for _ in range(n)]))
+
+sentences.sort() # 알파벳 순 정렬
+sentences.sort(key=len) # 길이 순 정렬
+
+for sentence in sentences:
+    output = sentence + '\n'
+    sys.stdout.write(output)
+    
