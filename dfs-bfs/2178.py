1f3e513d1b60f23994ee72e59845b56cf36f68c5
iloveslowfood <silkstaff@naver.com>
2021-03-16 13:02:19 +0900

    update README.md of Algorithms

--- dfs-bfs/2178.py
+++ dfs-bfs/2178.py
@@ -3,46 +3,67 @@
 0: 이동할 수 없는 칸
 '''
 import sys
+from collections import deque
 
-def move(loc, max_x, max_y):
-    moved_loc_list = []
-    dx_list = [0, 0, 1, 1]
-    dy_list = [0, 1, 0, 1]
+def move(loc, n: int, m: int):
+    '''loc: (x, y)
+    Up: (x, y-1)
+    Down: (x, y+1)
+    Left: (x-1, y)
+    Right: (x+1, y)
+    '''
+    move_list = []
+    dx_list = [0, 0, -1, 1]
+    dy_list = [-1, 1, 0, 0]
     for dx, dy in zip(dx_list, dy_list):
-        if (loc[0] - dx < 0) or (loc[0] - dx >= max_x) or (loc[1] - dy < 0) or (loc[1] - dy >= max_y):
+        if (loc[0] + dx < 0) or (loc[0] + dx >= n) or (loc[1] + dy < 0) or (loc[1] + dy >= m):
             continue
-        moved_loc = (loc[0] - dx, loc[1] - dy)
-        moved_loc_list.append(moved_loc)
-    return moved_loc_list
+        moved_loc = (loc[0] + dx, loc[1] + dy)
+        move_list.append(moved_loc)
+    return move_list
 
-n, m = map(int, input().split())
-START = (0, 0)
-ARRIVAL = (n-1, m-1)
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, list(sys.stdin.readline())[:-1])))
 
-visited = []
-num_paths = 0
-def dfs(graph: list, loc: tuple, arrival: tuple=ARRIVAL) -> int:
-    global n
-    global m
+def bfs(graph: list, n, m, loc: tuple, arrival: tuple) -> int:
     global num_paths
 
     if loc == arrival:
         return num_paths
 
+    move_list = deque([])
     for x, y in move(loc, n, m):
-        if ((x, y) not in visited) and (graph[x][y] == 1):
-            visited.append((x, y))
-            loc = (x, y) # 이동
-            dfs(graph, loc, arrival)
-            print(loc)
-            print(num_paths)
-            print(visited)
-        else:
-            print('all visit')
-
+        if ((x, y) not in visited) and (graph[x][y] == 1): # 방문한 곳이 아니고 벽이 아닌 경우
+            move_list.append((x, y))
+            # visited.append((x, y)) # 방문 처리
+            # loc = (x, y) # 이동
+            # num_paths += 1
+    
+    bfs(graph, n, m, loc, arrival)
     return num_paths
 
-print(dfs(graph, loc=START, arrival=ARRIVAL))
+
+# def dfs(graph: list, n, m, loc: tuple, arrival: tuple) -> int:
+#     global num_paths
+
+#     if loc == arrival:
+#         return num_paths
+
+#     for x, y in move(loc, n, m):
+#         if ((x, y) not in visited) and (graph[x][y] == 1): # 방문한 곳이 아니고 벽이 아닌 경우
+#             visited.append((x, y)) # 방문 처리
+#             loc = (x, y) # 이동
+#             num_paths += 1
+#             dfs(graph, n, m, loc, arrival)
+#     return num_paths
+
+n, m = map(int, input().split())
+START = (0, 0)
+ARRIVAL = (n-1, m-1)
+num_paths = 0
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, list(sys.stdin.readline())[:-1])))
+
+visited = [START]
+
+print(dfs(graph, n, m, START, ARRIVAL))
