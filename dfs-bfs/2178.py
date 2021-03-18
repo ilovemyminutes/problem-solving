b5c57123a5ee7efb89881ef2afb1e114132c30ff
iloveslowfood <silkstaff@naver.com>
2021-03-18 09:39:40 +0900

    solved 2178: 미로 탐색

--- dfs-bfs/2178.py
+++ dfs-bfs/2178.py
@@ -1,69 +1,44 @@
-'''
+'''https://www.acmicpc.net/problem/2178
 1: 이동할 수 있는 칸
 0: 이동할 수 없는 칸
 '''
 import sys
 from collections import deque
 
+
 def move(loc, n: int, m: int):
-    '''loc: (x, y)
-    Up: (x, y-1)
-    Down: (x, y+1)
-    Left: (x-1, y)
-    Right: (x+1, y)
-    '''
-    move_list = []
-    dx_list = [0, 0, -1, 1]
-    dy_list = [-1, 1, 0, 0]
+    movable_list = []
+    dx_list, dy_list  = [0, 0, -1, 1], [-1, 1, 0, 0]
     for dx, dy in zip(dx_list, dy_list):
         if (loc[0] + dx < 0) or (loc[0] + dx >= n) or (loc[1] + dy < 0) or (loc[1] + dy >= m):
             continue
         moved_loc = (loc[0] + dx, loc[1] + dy)
-        move_list.append(moved_loc)
-    return move_list
+        movable_list.append(moved_loc)
+    return movable_list
 
 
-def bfs(graph: list, n, m, loc: tuple, arrival: tuple) -> int:
-    global num_paths
+def bfs(graph: list, n, m) -> int:
+    start = (0, 0)
+    path = [[0] * m for _ in range(n)]
 
-    if loc == arrival:
-        return num_paths
+    que = deque([start])
+    path[start[0]][start[0]] = 1
 
-    move_list = deque([])
-    for x, y in move(loc, n, m):
-        if ((x, y) not in visited) and (graph[x][y] == 1): # 방문한 곳이 아니고 벽이 아닌 경우
-            move_list.append((x, y))
-            # visited.append((x, y)) # 방문 처리
-            # loc = (x, y) # 이동
-            # num_paths += 1
-    
-    bfs(graph, n, m, loc, arrival)
-    return num_paths
-
-
-# def dfs(graph: list, n, m, loc: tuple, arrival: tuple) -> int:
-#     global num_paths
-
-#     if loc == arrival:
-#         return num_paths
-
-#     for x, y in move(loc, n, m):
-#         if ((x, y) not in visited) and (graph[x][y] == 1): # 방문한 곳이 아니고 벽이 아닌 경우
-#             visited.append((x, y)) # 방문 처리
-#             loc = (x, y) # 이동
-#             num_paths += 1
-#             dfs(graph, n, m, loc, arrival)
-#     return num_paths
+    while que:
+        current = que.popleft()
+        for x, y in move(current, n, m):
+            if path[x][y] == 0 and graph[x][y] == 1:
+                path[x][y] = path[current[0]][current[1]] + 1
+                que.append((x, y))
+            
+    return path[-1][-1]
 
 n, m = map(int, input().split())
-START = (0, 0)
-ARRIVAL = (n-1, m-1)
-num_paths = 0
 
 graph = []
 for _ in range(n):
     graph.append(list(map(int, list(sys.stdin.readline())[:-1])))
 
-visited = [START]
+print(bfs(graph, n, m))
 
-print(dfs(graph, n, m, START, ARRIVAL))
+
