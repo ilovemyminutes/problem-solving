cebf0d2af5f4c73b29b4a560c761cae2631838a6
iloveslowfood <silkstaff@naver.com>
2021-04-11 16:35:10 +0900

    solved DP: 1003, BOJ

--- dp/1003.py
+++ dp/1003.py
@@ -1,38 +1,35 @@
 """피보나치 함수 https://www.acmicpc.net/problem/1003"""
 import sys
 
-MAX_N = 40
-zero_cnts = [0 for _ in range(MAX_N+1)]
-one_cnts = [0 for _ in range(MAX_N+1)]
 
-def count_zero_one(n):
-    global zero_cnt, one_cnt
-    if n == 0:
-        zero_cnts[n] += 1
-        zero_cnt = 1
-        one_cnt = 1
-        return zero_cnt, one_cnt
-    elif n == 1:
-        one_cnts[n] += 1
-        zero_cnt = 0
-        one_cnt = 1
-        return zero_cnt, one_cnt
+# [(zero_cnt for a_k, one_cnt for a_k) | k = 0, 1, ..., 40]
+dp = [None for _ in range(41)]
+
+def calc_zero_one(n: int) -> tuple:
+    global dp
+
+    if dp[n] is not None:
+        return dp[n]
     else:
-        if zero_cnts[n] != 0 and one_cnts[n] != 0:
-            return (zero_cnts[n], one_cnts[n])
+        if n == 0:
+            dp[n] = (1, 0)
+            return (1, 0)
+        elif n == 1:
+            dp[n] = (0, 1)
+            return (0, 1)
         else:
-            pre1 = count_zero_one(n-1)
-            pre2 = count_zero_one(n-2)
-            print(n, pre1, pre2)
-            zero_cnt = pre1[0] + pre2[0]
-            one_cnt = pre1[1] + pre2[1]
-            zero_cnts[n] = zero_cnt
-            one_cnts[n] = one_cnt
-            return zero_cnt, one_cnt
+            term1 = calc_zero_one(n-1)
+            dp[n-1] = term1
+            term2 = calc_zero_one(n-2)
+            dp[n-2] = term2
 
-# t = int(input())
-# counts = []
-# for _ in range(t):
-#     counts.append(count_zero_one(int(sys.stdin.readline())))
-print(count_zero_one(2))
-# print(zero_cnts, one_cnts)
+            # elementwise addition
+            output = (term1[0]+term2[0], term1[1]+term2[1])
+            dp[n] = output
+            return output
+    
+
+t = int(input())
+numbers = [int(input()) for _ in range(t)]
+for num in numbers:
+    print(*calc_zero_one(num))
